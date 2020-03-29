dace390213fbea7b63a984c70573d3754d7cd76e
Joshua Rodriguez <ewized@gmail.com>
2020-03-28 17:36:09 -0700

    Install webpack dev server

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -5,6 +5,17 @@ module.exports = {
   mode: 'production',
   devtool: 'source-map',
   target: 'node',
+  devServer: {
+    stats: {
+      children: false,
+      maxModules: 0,
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    port: 3001,
+    inline: true,
+  },
   // We are not running on the web so we dont need to download files, so changes reflect that
   optimization: {
     minimize: false,
