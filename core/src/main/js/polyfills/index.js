4c4b9a8719c5f8cd4fb7c012b2e649a19c66ab90
Joshua Rodriguez <ewized@gmail.com>
2020-04-05 10:43:54 -0700

    Take all exports and inject them into the globalThis

--- core/src/main/js/polyfills/index.js
+++ core/src/main/js/polyfills/index.js
@@ -1,9 +1,17 @@
 /*
  * Copyright 2020 Year4000. All Rights Reserved.
  */
-import { console as _console } from './polyfills/console.js'
-import { var_dump } from './polyfills/var_dump.js'
+import * as console_exports from './polyfills/console.js'
+import * as var_dump_exports from './polyfills/var_dump.js'
 
 
-globalThis.console = _console
-globalThis.var_dump = var_dump
+// Inject the exports into the globalThis variable
+[console_exports, var_dump_exports].forEach((exports) => {
+  Object.entries(exports).forEach(([key, value]) => {
+    try {
+      globalThis[key] = value
+    } catch (error) {
+      console?.error(error)
+    }
+  })
+})
